1df624d7bcc2e94d989258f4b6c7520cf42804ce
sandy <sandypalpan@gmail.com>
2013-08-20 14:31:58 -0500

    CAMBIAR CLAVE (DC, RF, RC)

--- app/modules/api/models/DbTable/Users.php
+++ app/modules/api/models/DbTable/Users.php
@@ -142,4 +142,27 @@ class Api_Model_DbTable_Users extends Zend_Db_Table_Abstract
         }
     }
 
+
+
+    public function _getFilter($where=null,$attrib=null,$orders=null){
+		try{
+			if($where['eid']=='' || $where['oid']=='') return false;
+				$select = $this->_db->select();
+				if ($attrib=='') $select->from("base_users");
+				else $select->from("base_users",$attrib);
+				foreach ($where as $atri=>$value){
+					$select->where("$atri = ?", $value);
+				}
+				foreach ($orders as $key => $order) {
+						$select->order($order);
+				}
+				$results = $select->query();
+				$rows = $results->fetchAll();
+				if ($rows) return $rows;
+				return false;
+		}catch (Exception $e){
+			print "Error: Read Filter Curricula ".$e->getMessage();
+		}
+	}
+
 }
