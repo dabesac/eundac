de3ed155415d6c8e5b27f218f471cb5826667c1a
sandy <sandypalpan@gmail.com>
2013-08-21 12:44:45 -0500

    PBI0028 VER HORARIO

--- app/modules/api/models/DbTable/Coursexteacher.php
+++ app/modules/api/models/DbTable/Coursexteacher.php
@@ -42,4 +42,25 @@ class Api_Model_DbTable_Coursexteacher extends Zend_Db_Table_Abstract
 		}
 	}
 
+	public function _getFilter($where=null,$attrib=null,$orders=null){
+		try{
+			if($where['eid']=='' || $where['oid']=='') return false;
+				$select = $this->_db->select();
+				if ($attrib=='') $select->from("base_course_x_teacher");
+				else $select->from("base_course_x_teacher",$attrib);
+				foreach ($where as $atri=>$value){
+					$select->where("$atri = ?", $value);
+				}
+				foreach ($orders as $key => $order) {
+						$select->order($order);
+				}
+				$results = $select->query();
+				$rows = $results->fetchAll();
+				if ($rows) return $rows;
+				return false;
+		}catch (Exception $e){
+			print "Error: Read Filter Curricula ".$e->getMessage();
+		}
+	}
+
 }
