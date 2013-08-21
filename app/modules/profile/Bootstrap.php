5c4d5f7a6cf4690b1e36be05a2072ce2dcb72b55
mgamarraparedes <maryielag5@gmail.com>
2013-08-21 16:29:33 -0500

    profile-syllabus

---
+++ app/modules/profile/Bootstrap.php
@@ -0,0 +1,25 @@
+<?php
+
+class Profile_Bootstrap extends Zend_Application_Module_Bootstrap 
+{
+    
+   protected function _initAutoload()
+    {
+        $autoloader = new Zend_Application_Module_Autoloader(array(
+            'namespace' => 'Bienestar_',
+            'basePath'  => APPLICATION_PATH .'/modules/profile',
+            'resourceTypes' => array (
+                'form' => array(
+                    'path' => 'forms',
+                    'namespace' => 'Form',
+                ),
+                'model' => array(
+                    'path' => 'models',
+                    'namespace' => 'Model',
+                ),
+            )
+        ));
+        return $autoloader;
+    }
+    }
+
