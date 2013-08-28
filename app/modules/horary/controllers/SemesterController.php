6e13d13cabe9b4f9bbe950172fc010bd04e67ff7
Sandro <sanliscorazoj@gamil.com>
2013-08-27 20:16:01 -0500

    horary semester advance

--- app/modules/horary/controllers/SemesterController.php
+++ app/modules/horary/controllers/SemesterController.php
@@ -1,8 +1,8 @@
 <?php
- class Horary_SemesterController extends Zend_Controller_Action{
+class Horary_SemesterController extends Zend_Controller_Action{
 
- 	public function init(){
- 		$sesion  = Zend_Auth::getInstance();
+	public function init(){
+		$sesion  = Zend_Auth::getInstance();
  		if(!$sesion->hasIdentity() ){
  			$this->_helper->redirector('index',"index",'default');
  		}
@@ -13,74 +13,61 @@
  		$this->sesion = $login;
  		require_once 'Zend/Loader.php';
         Zend_Loader::loadClass('Zend_Rest_Client');
- 	}
+	}
 
- 	public function indexAction(){
- 		try {
- 			$eid=$this->sesion->eid;
- 			$oid=$this->sesion->oid;
- 			$escid=$this->sesion->escid;
- 			$uid=$this->sesion->uid;
- 			$pid=$this->sesion->pid;
- 			$subid=$this->sesion->subid;
- 			$perid=$this->sesion->period->perid;
+	public function indexAction(){
+
+		try {
+			$eid=$this->sesion->eid;
+			$oid=$this->sesion->oid;
+			// $escid=$this->sesion->escid;
+			$escid='4SI';
+			// $perid='13B';
+			$perid=$this->sesion->period->perid;
+			$where=array('eid'=>$eid,'oid'=>$oid,'escid'=>$escid,'perid'=>$perid);
+			$sem= new Api_Model_DbTable_Semester();
+			$dsem=$sem->_getSemesterXPeriodsXEscid($where);
+			// print_r($dsem);exit();
+			$this->view->semester=$dsem;
+		} catch (Exception $e) {
+			print "Error: get semester".$e->getMessage();
+		}
+	}
+
+	public function horarysemesterAction(){
+		try {
+			$this->_helper->layout()->disableLayout();
+			$eid=$this->sesion->eid;
+			$oid=$this->sesion->oid;
+			// $escid=$this->sesion->escid;
+			$semid=$this->_getParam('semid');
+			$escid='4SI';
+			// $semid='3';
+			$subid=$this->sesion->subid;
+			$perid=$this->sesion->period->perid;
+			$this->view->semid=$semid;
+
 			$base_url = 'http://localhost:8080/';
-	        $endpoint = '/s1st3m4s/und4c/horary_student';
-	        $data = array('escid' => $escid,'eid' =>$eid,'oid' =>$oid,'perid'=>$perid,'subid'=>$subid,'uid'=>$uid);
+	        $endpoint = '/s1st3m4s/und4c/horary_course';
+	        $data = array('escid' => $escid,'eid' =>$eid,'oid' =>$oid,'perid'=>$perid,'subid'=>$subid,'semid'=>$semid);
 	        $client = new Zend_Rest_Client($base_url);
 	        $httpClient = $client->getHttpClient();
 	        $httpClient->setConfig(array("timeout" => 1800));
 	        $response = $client->restget($endpoint,$data);
 	        $lista=$response->getBody();
 	        $data = Zend_Json::decode($lista);
-        	// print_r($data);
         	$this->view->horarys=$data; 
- 			
- 		} catch (Exception $e) {
- 			print "Error: get Horary".$e->getMessage();
- 		}
- 	}
 
- 	public function printhoraryAction(){
- 		try {
- 			$eid=$this->sesion->eid;
-	        $oid=$this->sesion->oid;
-	        $escid=$this->sesion->escid;
-	        $facid=$this->sesion->facid;
-	        $uid=$this->sesion->uid;
-	        $pid=$this->sesion->pid;
-	        $subid=$this->sesion->subid;
-	        $perid=$this->sesion->period->perid;
-	        $this->view->uid=$uid;
+        	$where=array('eid'=>$eid,'oid'=>$oid,'perid'=>$perid,'escid'=>$escid,'semid'=>$semid);
+        	$cper= new Api_Model_DbTable_PeriodsCourses();
+        	$dcur=$cper->_getCoursesxPeriodxspecialityxsemester($where);
+        	$this->view->dcurso=$dcur;
+        	// print_r($dcur);exit();
+
 			
-			$base_url = 'http://localhost:8080/';
-	        $endpoint = '/s1st3m4s/und4c/horary_student';
-	        $data = array('escid' => $escid,'eid' =>$eid,'oid' =>$oid,'perid'=>$perid,'subid'=>$subid,'uid'=>$uid);
-	        $client = new Zend_Rest_Client($base_url);
-	        $httpClient = $client->getHttpClient();
-	        $httpClient->setConfig(array("timeout" => 1800));
-	        $response = $client->restget($endpoint,$data);
-	        $lista=$response->getBody();
-	        $data = Zend_Json::decode($lista);
-        	$this->view->horarys=$data;
-        	// print_r($data);
-        	$where=array('eid'=>$eid,'oid'=>$oid,'escid'=>$escid,'subid'=>$subid);
-        	$esc = new Api_Model_DbTable_Speciality();
-        	$desc = $esc->_getOne($where);
-        	$this->view->desc=$desc;
+		} catch (Exception $e) {
+			print "Error: get horary semester".$e->getMessage();
+		}
 
-        	$whered=array('eid'=>$eid,'oid'=>$oid,'facid'=>$facid);
-        	$fac= new Api_Model_DbTable_Faculty();
-        	$dfac = $fac->_getOne($whered);
-        	$this->view->dfac=$dfac;
-
-        	$wheres=array('eid'=>$eid,'oid'=>$oid,'escid'=>$escid,'uid'=>$uid,'subid'=>$subid,'pid'=>$pid); 
-        	$user = new Api_Model_DbTable_Users();
-        	$duser = $user->_getInfoUser($wheres);
-        	$this->view->duser=$duser;
-        	// print_r($duser); exit();			
- 		} catch (Exception $e) {
- 			print "Error: print Horary".$e->getMessage();
- 		}
- 	}
- }
+	}
+}
