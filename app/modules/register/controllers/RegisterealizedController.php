74a2dc4598a5d1a23c989b262425c811b3e8f181
Sandro <sanliscorazoj@gamil.com>
2013-09-02 18:52:32 -0500

    terminado de listar las matriculas realizadas

--- app/modules/register/controllers/RegisterealizedController.php
+++ app/modules/register/controllers/RegisterealizedController.php
@@ -25,15 +25,49 @@ class Register_RegisterealizedController extends Zend_Controller_Action {
 			$subid=$this->sesion->subid;
 			// $uid=$this->sesion->uid;
 			$escid='4SI';
- 			$uid='0924401019';
- 			$state='M';
- 			$states='C';
-			$where=array('eid'=>$eid,'oid'=>$oid,'escid'=>$escid,'subid'=>$subid,'uid'=>$uid,'state'=>$state,'state'=>$states);
+ 			$uid='0924403185';
+			$where=array('eid'=>$eid,'oid'=>$oid,'escid'=>$escid,'subid'=>$subid,'uid'=>$uid);
 			$attrib=array('escid','perid','courseid','turno','notafinal','state');
-	
+			$orders=array('perid','courseid');	
 			$dbgc= new Api_Model_DbTable_Registrationxcourse();
-			$data=$dbgc->_getFilter($where,$attrib,'courseid');
-			print_r($data);exit();
+			$data=$dbgc->_getFilter($where,$attrib,$orders);
+			$len=count($data);
+			$dbperiod= new Api_Model_DbTable_Periods();
+			$dbcourse=new Api_Model_DbTable_Course();
+			
+			$newperiod=array();
+			$c=0;
+			$aux=$data[0]['perid'];
+			$newperiod[0]=$aux;
+			foreach ($data as $periods) {
+				$perid=$periods['perid'];				 
+				if ($perid!=$aux) {
+					$c++;	
+					$newperiod[$c]=$perid;
+					$aux=$perid;
+				}
+			}
+			$l=count($newperiod);
+			for ($f=0; $f < $l ; $f++) { 
+				$whered=array('eid'=>$eid,'oid'=>$oid,'perid'=>$newperiod[$f]);
+				$attrib=array('perid','name');
+				$dasd=$dbperiod->_getFilter($whered,$attrib);
+				$newperiod[$f]=$dasd[0];
+			}
+			// print_r($newperiod);s
+			for ($i=0; $i < $len; $i++) {
+				$courseid=$data[$i]['courseid'];
+				$whered=array('eid'=>$eid,'oid'=>$oid,'escid'=>$escid,'subid'=>$subid,'courseid'=>$courseid);
+				$attrib=array('courseid','name','semid','credits');
+				$datac=$dbcourse->_getFilter($whered,$attrib);
+				$datacourse[$i]=$datac[0];
+			}
+			// print_r($datacourse);
+			// print_r($data);exit();
+			$this->view->newperiod=$newperiod;
+			$this->view->datap=$datap;
+			$this->view->datacourse=$datacourse;
+			$this->view->data=$data;
 			
 		} catch (Exception $e) {
 			print "Error: get Registers".$e->getMessage();
