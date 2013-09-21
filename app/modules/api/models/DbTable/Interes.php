32f6d734f692fa5b9f0cb7e2f5b16599a5f75f31
Liam Klyneker <Stayner_93@hotmail.com>
2013-09-20 19:42:42 -0500

    Editar Datos del perfil 80 %

--- app/modules/api/models/DbTable/Interes.php
+++ app/modules/api/models/DbTable/Interes.php
@@ -4,6 +4,21 @@ class Api_Model_DbTable_Interes extends Zend_Db_Table_Abstract
 {
 	protected $_name = 'base_interes';
 	protected $_primary = array("iid","eid","pid");
+	protected $_sequence ="s_interes";
+
+	public function _getOne($where=null){
+		try{
+			
+			if ($where['iid']=="" || $where['eid']=="" || $where['pid']=="") return false;
+			
+			$wherestr="iid = '".$where['iid']."' and eid = '".$where['eid']."' and pid = '".$where['pid']."'";
+			$row = $this->fetchRow($wherestr);
+			if($row) return $row->toArray();
+			return false;
+		}catch (Exception $e){
+			print "Error: Read One Interes ".$e->getMessage();
+		}
+	}
 
 	public function _getFilter($where=null,$attrib=null,$orders=null){
 		try{
@@ -26,6 +41,28 @@ class Api_Model_DbTable_Interes extends Zend_Db_Table_Abstract
 				return false;
 		}catch (Exception $e){
 			print "Error: Read Filter Intereses ".$e->getMessage();
+		}
+	}
+
+	public function _save($data){
+		try{
+			if ($data['eid']=='' || $data['pid']=='') return false;
+			return $this->insert($data);
+			return false;
+		}catch (Exception $e){
+			print "Error al Guardar Interes ".$e->getMessage();
+		}
+	}
+
+
+	public function _delete($pk){
+		try{
+			if ($pk['eid']=='' || $pk['pid']==''|| $pk['iid']=='') return false;
+			$where = "eid = '".$pk['eid']."'and pid = '".$pk['pid']."'and iid = '".$pk["iid"]."'";
+			return $this->delete($where);
+			return false;
+		}catch (Exception $e){
+			print "Error: Delete Interes ".$e->getMessage();
 		}
 	}
 }
