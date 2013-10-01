389f02458642b8ec6b8467a77f7b4b961bcfa7a8
Sandro <sanliscorazoj@gamil.com>
2013-09-30 20:30:32 -0500

    terminado open records

---
+++ app/modules/admin/controllers/OpensillabusController.php
@@ -0,0 +1,64 @@
+<?php 
+class Admin_OpensillabusController extends Zend_Controller_Action{
+
+	public function init(){
+		$sesion  = Zend_Auth::getInstance();
+ 		if(!$sesion->hasIdentity() ){
+ 			$this->_helper->redirector('index',"index",'default');
+ 		}
+ 		$login = $sesion->getStorage()->read();
+ 		$this->sesion = $login;
+	}
+
+	public function indexAction(){
+		try {
+			$fm= new Admin_Form_Openrecords();
+			$this->view->fm=$fm;
+		} catch (Exception $e) {
+			print "Error: Opensillabus".$e->getMessage();
+		}
+	}
+
+	public function lperiodAction(){
+		$this->_helper->layout()->disableLayout();
+		$eid=$this->sesion->eid;
+		$oid=$this->sesion->oid;
+		$anio=$this->_getParam('anio');
+		$a= substr($anio, 2, 4);
+		$data=array('eid'=>$eid,'oid'=>$oid,'year'=>$a);
+		$dbperiod= new Api_Model_DbTable_Periods();
+		$dataperiod = $dbperiod->_getPeriodsxYears($data);
+		$this->view->dataperiod=$dataperiod;
+	}
+
+	public function coursesAction(){
+		try {
+			$this->_helper->layout()->disableLayout();
+			$eid=$this->sesion->eid;
+			$oid=$this->sesion->oid;
+			$perid = $this->_getParam('perid');
+			$subid = $this->_getParam('subid');
+			$escid = $this->_getParam('escid');
+			$where=array('eid'=>$eid,'oid'=>$oid,'perid'=>$perid,'subid'=>$subid,'escid'=>$escid);
+			$attrib=array('perid','courseid','curid','turno','teach_uid','teach_pid','state');
+			$orders=array('courseid','turno');
+			$dbcourses= new Api_Model_DbTable_Syllabus();
+			$datacourses = $dbcourses->_getFilter($where,$attrib,$orders);
+			$i=0;
+			foreach ($datacourses as $course) {
+				$curid=$course['curid'];
+				$courseid=$course['courseid'];
+				$where=array('eid'=>$eid,'oid'=>$oid,'curid'=>$curid,'escid'=>$escid,'subid'=>$subid,'courseid'=>$courseid);
+				$attrib=array('courseid','name','semid');
+				$dbcourse = new Api_Model_DbTable_Course();
+				$datacourse[$i]= $dbcourse->_getFilter($where,$attrib);
+				$i++;
+			}
+			// print_r($datacourse);
+			$this->view->datacourses=$datacourses;
+			$this->view->datacourse=$datacourse;
+		} catch (Exception $e) {
+			print "Error: get Sillabus".$e->getMessage();
+		}
+	}
+}
