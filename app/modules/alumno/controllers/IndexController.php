17866486c5378ad81e0df3b8e154660b42a06f33
mgamarraparedes <maryielag5@gmail.com>
2013-10-01 13:21:18 -0500

    grafica asistencia

--- app/modules/alumno/controllers/IndexController.php
+++ app/modules/alumno/controllers/IndexController.php
@@ -63,28 +63,68 @@ class Alumno_IndexController extends Zend_Controller_Action {
         try
         {
             $this->_helper->layout()->disableLayout();         
-            $where['uid']=$this->sesion->uid;
-            $where['eid']=$this->sesion->eid;
-            $where['oid']=$this->sesion->oid;
-            $where['pid']=$this->sesion->pid;
-            $where['escid']=$this->sesion->escid;
-            $where['subid']=$this->sesion->subid;
-            $this->view->escid = $where['escid'];
+            $wheres['uid']=$this->sesion->uid;
+            $wheres['eid']=$this->sesion->eid;
+            $wheres['oid']=$this->sesion->oid;
+            $wheres['pid']=$this->sesion->pid;
+            $wheres['escid']=$this->sesion->escid;
+            $wheres['subid']=$this->sesion->subid;
+            $wheres['perid']=$this->sesion->period->perid;
 
-            $this->view->uid = $where['uid'];
-            $this->view->oid = $where['oid'];
-            $this->view->eid = $where['eid'];
-            $dbcurricula=new Api_Model_DbTable_Studentxcurricula();
-            $datcur=$dbcurricula->_getOne($where);
-            $where['curid']=$datcur['curid'];
-            $this->view->curid = $where['curid'];
-            $dbcursos=new Api_Model_DbTable_Course();
-            $datcursos=$dbcursos->_getCountCoursesxSemester($where);
-            // print_r($datcursos);
-            $this->view->data=$datcursos;
-            $cur=$dbcursos->_getCountCoursesxApproved($where);
-            // print_r($cur);
-            $this->view->cursos=$cur;
+            $this->view->escid = $wheres['escid'];
+            $this->view->uid = $wheres['uid'];
+            $this->view->oid = $wheres['oid'];
+            $this->view->eid = $wheres['eid'];
+                       // //Obtenemos los cursos matriculados
+            $lcursos = new Api_Model_DbTable_StudentAssistance();
+            $listacurso =$lcursos->_assistence($wheres);
+            $j=0;
+            foreach ($listacurso as $cursomas){
+                $where[$j]['eid']=$wheres["eid"];
+                $where[$j]['oid']=$wheres["oid"];
+                $where[$j]['escid']=$wheres["escid"];
+                $where[$j]['subid']=$wheres["subid"];
+                $where[$j]['perid']=$wheres["perid"];
+
+                $where[$j]['courseid']=$cursomas["coursoid"];
+                $where[$j]['name']=$cursomas["name"];
+                $where[$j]['curid']=$cursomas["curid"];
+                $where[$j]['turno']=$cursomas["turno"];
+                $periods = new Api_Model_DbTable_PeriodsCourses();
+                $state =$periods->_getOne($where);
+                $var=$state['state'];
+                if($var=='A'){
+                    $a=1;
+                }
+                if($var=='P'){
+                     $a=18;
+                }      
+                $x=0;
+                $x1=0;
+                for ($i=$a; $i < 35 ; $i++) { 
+                        $assis=$cursomas["a_sesion_".$i];
+                    if ($assis=='A' or $assis=='T') {
+                        $x++;
+                    }
+                     if ($assis=='F') {
+                        $x1++;
+                    }
+                }
+                $where[$j]['asistio']=$x;
+                $where[$j]['falto']=$x1;
+
+                if ($x1>=6) {
+                $where[$j]['coment']='R';
+                    }
+                else{
+                $where[$j]['coment']='N';
+
+                }
+
+                   $j++;
+        } 
+        $this->view->assistence=$where; 
+
         }
         catch(Exception $ex)
         {
