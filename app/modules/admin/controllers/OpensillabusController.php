c8114c7dd587b9adceecdf8dc0258c7979adb5e6
Sandro <sanliscorazoj@gamil.com>
2013-10-01 12:07:47 -0500

    terminado abrir silabos

--- app/modules/admin/controllers/OpensillabusController.php
+++ app/modules/admin/controllers/OpensillabusController.php
@@ -40,7 +40,7 @@ class Admin_OpensillabusController extends Zend_Controller_Action{
 			$subid = $this->_getParam('subid');
 			$escid = $this->_getParam('escid');
 			$where=array('eid'=>$eid,'oid'=>$oid,'perid'=>$perid,'subid'=>$subid,'escid'=>$escid);
-			$attrib=array('perid','courseid','curid','turno','teach_uid','teach_pid','state');
+			$attrib=array('perid','subid','escid','courseid','curid','turno','teach_pid','state','created');
 			$orders=array('courseid','turno');
 			$dbcourses= new Api_Model_DbTable_Syllabus();
 			$datacourses = $dbcourses->_getFilter($where,$attrib,$orders);
@@ -48,17 +48,48 @@ class Admin_OpensillabusController extends Zend_Controller_Action{
 			foreach ($datacourses as $course) {
 				$curid=$course['curid'];
 				$courseid=$course['courseid'];
+				$turno=$course['turno'];
 				$where=array('eid'=>$eid,'oid'=>$oid,'curid'=>$curid,'escid'=>$escid,'subid'=>$subid,'courseid'=>$courseid);
 				$attrib=array('courseid','name','semid');
 				$dbcourse = new Api_Model_DbTable_Course();
 				$datacourse[$i]= $dbcourse->_getFilter($where,$attrib);
+				$whered=array('eid'=>$eid,'oid'=>$oid,'perid'=>$perid,'subid'=>$subid,'escid'=>$escid,'courseid'=>$courseid,'curid'=>$curid,'turno'=>$turno);
+				$attrib=array('type_rate');
+				$orders=array('courseid','turno');
+				$dbcour= new Api_Model_DbTable_PeriodsCourses();
+				$datacour[$i] = $dbcour->_getFilter($whered,$attrib,$orders);
 				$i++;
 			}
-			// print_r($datacourse);
+				// print_r($datacourses);
+			$this->view->datacour=$datacour;	
 			$this->view->datacourses=$datacourses;
 			$this->view->datacourse=$datacourse;
 		} catch (Exception $e) {
 			print "Error: get Sillabus".$e->getMessage();
 		}
+	}
+
+	public function updatestateAction(){
+		try {
+			$this->_helper->layout()->disableLayout();
+			$eid=$this->sesion->eid;
+			$oid=$this->sesion->oid;
+			$courseid = base64_decode($this->_getParam('courseid'));
+			$perid = base64_decode($this->_getParam('perid'));
+			$escid = base64_decode($this->_getParam('escid'));
+			$curid = base64_decode($this->_getParam('curid'));
+			$turno = base64_decode($this->_getParam('turno'));
+			$subid = base64_decode($this->_getParam('subid'));
+			$state = base64_decode($this->_getParam('state'));
+			$pk=array('eid'=>$eid,'oid'=>$oid,'perid'=>$perid,'courseid'=>$courseid,'escid'=>$escid,'subid'=>$subid,'curid'=>$curid,'turno'=>$turno);
+			$data=array('state'=>$state);
+			// print_r($pk);
+			// print_r($data);
+			$bdsyllabus= new Api_Model_DbTable_Syllabus();
+			$updatedata= $bdsyllabus->_update($data,$pk);
+		} catch (Exception $e) {
+			print "Error: Open Records".$e->getMessage();
+		}
+
 	}
 }
