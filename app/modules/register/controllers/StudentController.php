a3495e4da0cbd311f964bfb18bed62e64bb7908e
Henry Garcia <henry@yaroslab.com>
2013-10-10 16:36:27 -0500

    Active pre registration student last 1324507145 -0822277014  0591203040

--- app/modules/register/controllers/StudentController.php
+++ app/modules/register/controllers/StudentController.php
@@ -11,7 +11,7 @@ class Register_StudentController extends Zend_Controller_Action {
          $login = $sesion->getStorage()->read();
         $this->sesion = $login;
         if ($login->uid=='9811103229' || $login->uid=='1324507145' || $login->uid=='0822277014'
-			|| $login->uid=='1324507096'){
+			|| $login->uid=='1324507096' || $login->uid=='0591203040'){
 
         }else {
         	$this->_helper->redirector('index',"index",'default');
