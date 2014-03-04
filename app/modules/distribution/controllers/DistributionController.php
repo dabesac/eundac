4e73942068c49c00d52d07c09b5ed207b6c8ebd1
Paolo <sbautistacalderon>
2014-03-04 13:03:14 -0500

    impresion reporte academico de cada docente

--- app/modules/distribution/controllers/DistributionController.php
+++ app/modules/distribution/controllers/DistributionController.php
@@ -871,6 +871,7 @@ class Distribution_DistributionController extends Zend_Controller_Action {
             $pid=base64_decode($this->_getParam('pid'));
             $perid=base64_decode($this->_getParam('perid'));
             $escid=base64_decode($this->_getParam('escid'));
+            $subid=base64_decode($this->_getParam('subid'));
             $distid=base64_decode($this->_getParam('distid'));
             $where=array('eid'=>$eid,'oid'=>$oid,'uid'=>$uid,'pid'=>$pid,'perid'=>$perid,'distid'=>$distid);
             $orders=array('curid','courseid','turno');
@@ -914,9 +915,66 @@ class Distribution_DistributionController extends Zend_Controller_Action {
             $dataperson1 = $bdperson->_getOne($where3);
             $this->view->dataper1=$dataperson1;
 
-            // print_r($dataperson);exit();
+            $where=array('eid'=>$eid,'oid'=>$oid,'escid'=>$escid,'subid'=>$subid);
+            $base_speciality =  new Api_Model_DbTable_Speciality();        
+            $speciality = $base_speciality ->_getOne($where);
+            $parent=$speciality['parent'];
+            $wher=array('eid'=>$eid,'oid'=>$oid,'escid'=>$parent,'subid'=>$subid);
+            $parentesc= $base_speciality->_getOne($wher);
+
+            if ($parentesc) {
+                $pala='ESPECIALIDAD DE ';
+                $spe['esc']=$parentesc['name'];
+                $spe['parent']=$pala.$speciality['name'];
+            }
+            else{
+                $spe['esc']=$speciality['name'];
+                $spe['parent']='';  
+            }
+            $names=strtoupper($spe['esc']);
+            $namep=strtoupper($spe['parent']);
+            $namev=$names." ".$namep;
+            $this->view->namev=$namev;
+            $namefinal=$names." <br> ".$namep;
+            $namelogo = (!empty($speciality['header']))?$speciality['header']:"blanco";
+            
+            $fac = array('eid'=>$eid,'oid'=>$oid,'facid'=>$speciality['facid']);
+            $base_fac =  new Api_Model_DbTable_Faculty();        
+            $datafa= $base_fac->_getOne($fac);
+            $namef = strtoupper($datafa['name']);
+
+            $dbimpression = new Api_Model_DbTable_Countimpressionall();
+    
+            // $uid=$this->sesion->uid;
+            $uidim=$this->sesion->pid;
+            // $pid=$uidim;
+
+            $data = array(
+                'eid'=>$eid,
+                'oid'=>$oid,
+                'uid'=>$uid,
+                'escid'=>$escid,
+                'subid'=>$subid,
+                'pid'=>$pid,
+                'type_impression'=>'impresion_carga_acacemica',
+                'date_impression'=>date('Y-m-d H:i:s'),
+                'pid_print'=>$uidim
+                );
+            $dbimpression->_save($data);            
+
+            $wheri = array('eid'=>$eid,'oid'=>$oid,'uid'=>$uid,'pid'=>$pid,'escid'=>$escid,
+                'subid'=>$subid,'type_impression'=>'impresion_carga_acacemica');
+            $dataim = $dbimpression->_getFilter($wheri);
+                        
+            $co=count($dataim);            
+            $codigo=$co." - ".$uidim;
             $header=$this->sesion->org['header_print'];
             $footer=$this->sesion->org['footer_print'];
+            $header = str_replace("?facultad",$namef,$header);
+            $header = str_replace("?escuela",$namefinal,$header);
+            $header = str_replace("?logo", $namelogo, $header);
+            $header = str_replace("?codigo", $codigo, $header);
+
             $this->view->header=$header;
             $this->view->footer=$footer;
             $this->view->perid=$perid;
