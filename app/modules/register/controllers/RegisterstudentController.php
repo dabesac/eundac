b95ce3f883ed556df4be66a9d93ea78b16077151
Liam <Stayner93@gmail.com>
2014-03-04 10:35:48 -0500

    Actuañlizacion

--- app/modules/register/controllers/RegisterstudentController.php
+++ app/modules/register/controllers/RegisterstudentController.php
@@ -669,7 +669,7 @@ class Register_RegisterstudentController extends Zend_Controller_Action {
         $oid   = $this->sesion->oid;
         $perid = $this->sesion->period->perid;
 
-        $where = array( 'eid'   => $eid,
+        /*$where = array( 'eid'   => $eid,
                         'oid'   => $oid,
                         'perid' => $perid,
                         'escid' => base64_decode($escid),
@@ -678,7 +678,7 @@ class Register_RegisterstudentController extends Zend_Controller_Action {
                         'uid'   => base64_decode($uid) );
         $attrib = array('courseid');
         $courses = $coursesDb->_getFilter($where, $attrib);
-        $this->view->courses = $courses;
+        $this->view->courses = $courses;*/
 
         $data = array(  'eid' => base64_encode($eid),
                         'oid' => base64_encode($oid),
