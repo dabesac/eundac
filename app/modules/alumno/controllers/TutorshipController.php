ff854d3cf276e5917266a0f604325e5ffa9e792e
Liam <Stayner93@gmail.com>
2014-04-08 18:13:57 -0500

    Just Pull

--- app/modules/alumno/controllers/TutorshipController.php
+++ app/modules/alumno/controllers/TutorshipController.php
@@ -13,7 +13,22 @@ class Alumno_TutorshipController extends Zend_Controller_Action {
     }
 
     public function listtutorsAction(){
-    	
+    	$escid = $this->sesion->escid;
+
+        $query = array(
+	                 array(	'column'   =>'author',
+							'operator' =>'=',
+							'value'    =>$this->sesion->infouser['numdoc'] )
+            		);
+
+        /*$data_project = array();
+        $connect = new Eundac_Connect_openerp();
+        $ids = $connect->search('inv.pro.project',$query);
+        if ($ids) {
+            $data_project = $connect->read($ids, $attributes, 'inv.pro.project');
+        }
+        print_r($query);
+        $this->view->data_project=$data_project;*/
     }
 
 }
