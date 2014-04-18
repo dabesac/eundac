40664bf329847dfda8feccb4a20794a7b2576b56
Liam Klyneker <Stayner93@gmail.com>
2014-04-18 18:59:44 -0500

    Just Pull

---
+++ app/modules/alumno/controllers/ProjectsController.php
@@ -0,0 +1,21 @@
+<?php
+
+class Alumno_ProjectsController extends Zend_Controller_Action {
+
+    public function init()
+    {
+    	$sesion  = Zend_Auth::getInstance();
+    	if(!$sesion->hasIdentity() ){
+    		$this->_helper->redirector('index',"index",'default');
+    	}
+    	$login = $sesion->getStorage()->read();
+    	$this->sesion = $login;
+
+    }
+
+    public function listprojectsAction(){
+    	$serverErp = new Eundac_Connect_openerp();
+    	
+    }
+
+}
