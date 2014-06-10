f4c097862ce0d5af4519cef6bcb3d91df80d9bba
gersoneufra <gersoneufra@gmail.com>
2014-06-10 10:12:36 -0500

    registarion

---
+++ app/modules/services/controllers/IndexController.php
@@ -0,0 +1,24 @@
+<?php
+
+class Services_IndexController extends Zend_Controller_Action {
+
+    public function init()
+    {
+       $sesion  = Zend_Auth::getInstance();
+        if(!$sesion->hasIdentity() ){
+            $this->_helper->redirector('index',"index",'default');
+        }
+        $login = $sesion->getStorage()->read();
+        $this->sesion = $login;
+    }
+
+    public function indexAction()
+    {  
+       
+
+    }
+
+    public function dinerAction(){
+        
+    }
+}
