b7849c586052393593a589aa899b6defe0499a23
chriro <cristian_111_2@hotmail.com>
2014-07-18 16:13:23 -0500

    Curso de dirigido

--- app/modules/docente/controllers/NotasController.php
+++ app/modules/docente/controllers/NotasController.php
@@ -70,12 +70,15 @@ class Docente_NotasController extends Zend_Controller_Action{
 		$base_period = new Api_Model_DbTable_Periods();
 	    $data_period = $base_period->_getOnePeriod($where);
 	    $period_tm_act_ini = $base_period->_get_periods_ini_temp_activo($where);
-	    
+
 
 	    $li = '';
 		 if ($period_tm_act_ini) {
 		 	foreach ($period_tm_act_ini as $key => $value) {
-                $li = "<li><a href=/docente/notas/index/perid/".base64_encode($value['perid']).">".$value['name']." | ".$value['perid']."</a></li> \n" . $li;
+		 		$p = substr($value['perid'], 2, 1);
+		 		if($p!='J'){
+                	$li = "<li><a href=/docente/notas/index/perid/".base64_encode($value['perid']).">".$value['name']." | ".$value['perid']."</a></li> \n" . $li;
+                }
 			}
 		}
 	    $this->view->periods = $li;
