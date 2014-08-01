b0a8c93ef07434b3643f80c2732f99b35639089e
Liam Klyneker <Stayner93@gmail.com>
2014-08-01 13:20:45 -0500

    Fix Avance Academico, ese modulo no funcionaba para nada y hasta hubo reclamos, me tomo toda la mañana ¬¬

--- app/modules/default/controllers/SendmailController.php
+++ app/modules/default/controllers/SendmailController.php
@@ -76,13 +76,102 @@ class SendmailController extends Zend_Controller_Action{
       $this->view->dataSpeciality = $dataSpeciality;
    }
 
-   public function enviarAction(){
+   public function sendAction(){
+      //DataBases
+      $userDb   = new Api_Model_DbTable_Users();
+      $personDb = new Api_Model_DbTable_Person();
+
+      $this->_helper->layout()->disableLayout();
+      /*$eid = $this->sesion->eid;
+      $oid = $this->sesion->oid;
+
+      $formData = $this->getRequest()->getPost();
+      if (  $formData['selectFaculty'] != '' 
+            and $formData['selectDestinatarios'] != '' 
+            and $formData['tituloCorreo'] != ''
+            and $formData['cuerpoCorreo'] != '') {
+         if ($formData['selectSpecilaty']) {
+            $search = 'escid';
+            $escid  = base64_decode($formData['selectSpecilaty']);
+         }elseif ($formData['selectSchool']) {
+            $search = 'escid';
+            $escid  = base64_decode($formData['selectSchool']);
+         }else{
+            $escid  = base64_decode($formData['selectFaculty']);
+            $cantCF = strlen($escid);
+            $search = 'left(escid, '.$cantCF.')';
+         }
+
+         if ($escid == 'TODO') {
+            $where = array('eid'   => $eid,
+                           'oid'   => $oid,
+                           'rid'   => $formData['selectDestinatarios'],
+                           'state' => 'A' );
+         }else{
+            $where = array('eid'   => $eid,
+                           'oid'   => $oid,
+                           $search => $escid,
+                           'rid'   => $formData['selectDestinatarios'],
+                           'state' => 'A' );
+         }
+
+         $attrib = array('pid');
+         $preDataUsers = $userDb->_getFilter($where, $attrib);
+         $cPersons = 0;
+         foreach ($preDataUsers as $user) {
+            $where = array('eid' => $eid,
+                           'pid' => $user['pid'] );
+            $attrib = array('last_name0', 'last_name1', 'first_name', 'mail_person');
+
+            $preDataPerson = $personDb->_getFilter($where, $attrib);
+            if ($preDataPerson[0]['mail_person']) {
+               $mail = $preDataPerson[0]['mail_person'];
+               $tamCorreo = strlen($mail);
+               $isMail = 'no';
+               for ($i=0; $i < $tamCorreo; $i++) { 
+                  if ($mail[$i] == '@') {
+                     $isMail = 'yes';
+                  }
+               }
+               if ($isMail == 'yes') {
+                  $dataPerson[$cPersons]['fullName'] = $preDataPerson[0]['last_name0'].' '.$preDataPerson[0]['last_name1'].' '.$preDataPerson[0]['first_name'];
+                  $dataPerson[$cPersons]['mail']     = $preDataPerson[0]['mail_person'];
+                  $cPersons++;
+               }
+            }
+         }
+
+         //Enviar Correo :D
+         print_r($formData);
+         /*$mail->addTo('poolrojasm@gmail.com', 'Pol');
+         $mail->addTo('sanliscorazon@gmail.com', 'Sandro');
+         foreach ($dataPerson as $person) {
+            $mail->addTo($person['mail'], $person['fullName']);
+         }*/
+         /*if ($mail->send()) {
+            //$result = array('success' => 1);
+         }else{
+            //$result = array('success' => 0);
+         }
+         print(json_encode($result));
+      }*/
+      $config = array(  'ssl'      => 'tls', 
+                        'port'     => 587, 
+                        'auth'     => 'login', 
+                        'username' => 'informatica@undac.edu.pe', 
+                        'password' => 'jkskse93' );
+
+      $mailTransport = new Zend_Mail_Transport_Smtp('smtp.gmail.com', $config); 
+      Zend_Mail::setDefaultTransport($mailTransport);
+ 
       $mail = new Zend_Mail();
-      $mail->setBodyText('My Nice Test Text');
-      $mail->setBodyHtml('My Nice <b>Test</b> Text');
-      $mail->setFrom('somebody@example.com', 'Some Sender');
-      $mail->addTo('somebody_else@example.com', 'Some Recipient');
+      $mail->setBodyText('This is the text of the mail.');
+      $mail->addTo('stayner93@gmail.com', 'Liam Klyneker');
       $mail->setSubject('TestSubject');
-      $mail->send();
+      if ($mail->send()) {
+         print_r(1);
+      }else{
+         print_r(0);
+      };
    }
 }
