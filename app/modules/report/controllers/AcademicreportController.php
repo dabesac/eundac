d5b1864ef6e538986a2546e8fd13871776a4bd42
chriro <cristian_111_2@hotmail.com>
2014-11-07 12:04:54 -0500

    reporte academico para RC

--- app/modules/report/controllers/AcademicreportController.php
+++ app/modules/report/controllers/AcademicreportController.php
@@ -25,12 +25,82 @@ class Report_AcademicreportController extends Zend_Controller_Action{
 			print "Error: ".$e->getMessage();
 		}
 	}
+	 public function lperiodoAction(){
+        try {
+            $this->_helper->layout()->disableLayout();
+            $eid=$this->sesion->eid;
+            $oid=$this->sesion->oid;
+            $anio=$this->_getParam('anio');
+            //$anio = $this->getParam('anio');
+            $anio = substr($anio,-2);
+            $periodosDb = new Api_Model_DbTable_Periods();
+            $where = array('eid'=>$eid, 'oid'=>$oid, 'year'=>$anio);
+            //print_r($where);
+            $periods = $periodosDb->_getPeriodsxYears($where);
+            $this->view->periods = $periods;
 
+            } catch (Exception $e) {
+                print 'Error '.$e->getMessage();
+            }
+        }
 	public function schoolsAction(){
 		try{
-			
+			$this->_helper->layout()->disableLayout();
+			$facid=base64_decode($this->getParam('facid'));
+			$eid = $this->sesion->eid;
+			$oid = $this->sesion->oid;
+			$consulschool = new Api_Model_DbTable_Speciality();
+			$where = array('eid'=>$eid,'oid'=>$oid,'facid'=>$facid);
+			$dataschool = $consulschool->_getSchoolXFacultyNOTParent($where);
+			$this->view->schools=$dataschool;
 		}catch(Exception $e){
 			print 'Error; '.$e->getMessage();
+		}
+	}
+	public function specialityAction(){
+		try{
+			$this->_helper->layout()->disableLayout();
+			$escid=base64_decode($this->getParam('escid'));
+			$subid=base64_decode($this->getParam('subid'));
+			$eid = $this->sesion->eid;
+			$oid = $this->sesion->oid;
+			$where = array('eid' => $eid, 'oid' => $oid, 'parent' => $escid);
+            $es = new Api_Model_DbTable_Speciality();
+            $especia = $es->_getFilter($where,$attrib=null,$orders=null);
+            $this->view->especialidad=$especia;
+
+		}catch(Exception $e){
+			print "Error: ".$e->getMessage();
+		}
+	}
+
+	public function listAction(){
+		try{
+			$this->_helper->layout()->disableLayout();
+			$eid=$this->sesion->eid;
+            $oid=$this->sesion->oid;
+			$data = $this->getRequest()->getPost();
+			$where = array('escid'=>$data['escid'],'perid'=>$data['perid']);
+			$consul = new Api_Model_DbTable_Registrationxcourse();
+			$conspaciality = new Api_Model_DbTable_Speciality();
+
+			if($data['opcion']==1){
+				$resul= $consul->_firstlegends($where);
+			}elseif($data['opcion']==2){
+				$resul= $consul->_firstlegendsirregulares($where);
+			}elseif($data['opcion']==3){
+				$resul= $consul->_firstlegendsretirados($where);
+			}
+			
+			$wher = array('eid'=>$eid,'oid'=>$oid);
+			$speciality = $conspaciality->_getFilter($wher);
+			$this->view->resp=$resul;
+			$this->view->data=$speciality;
+			$this->view->opcion=$data['opcion'];
+			$this->view->periodo=$data['perid'];
+
+		}catch(Excepiton $e){
+			print "Error: ".$e->getMessage();
 		}
 	}
 }
