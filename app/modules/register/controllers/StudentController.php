522253e3b8a7a93f5034c1c8bc5fb111d7b566e9
Paolo Bautista <sanliscorazon@gmail.com>
2015-03-23 17:50:17 -0500

    corregido actas visualizacion

--- app/modules/register/controllers/StudentController.php
+++ app/modules/register/controllers/StudentController.php
@@ -64,7 +64,7 @@ class Register_StudentController extends Zend_Controller_Action {
                     'escid'=>$escid,
                     'subid'=>$subid,
                     'perid'=>$perid,
-                    'ratid'=>20,
+                    'ratid'=>39,
                     'amount'=>0,
                     'register'=>$uid,
                     'created'=>date('Y-m-d H:m:s'),
