2b759c61c7490ac79fc355b5b65f43bd54dbc0a9
Liam Klyneker <Stayner93@gmail.com>
2015-03-30 12:27:09 -0500

    Fix jQuery para asignar cursos

--- app/modules/distribution/controllers/IndexController.php
+++ app/modules/distribution/controllers/IndexController.php
@@ -4,12 +4,18 @@ class Distribution_IndexController extends Zend_Controller_Action {
 
     public function init()
     {
-    	
+    	$sesion  = Zend_Auth::getInstance();
+    	if(!$sesion->hasIdentity() ){
+    		$this->_helper->redirector('index',"index",'default');
+    	}
+    	$login = $sesion->getStorage()->read();
+    	if (($login->infouser['teacher']['is_director']<>"S")){
+    		$this->_helper->redirector('index','error','default');
+    	}
+    	$this->sesion = $login;
     }
     public function indexAction()
     {
-    
-    	
-
+    	// print_r('Distribución');
     }
 }
